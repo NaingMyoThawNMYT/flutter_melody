28fc51f0af21666bc7bcc3b91496243ce41d56b6
Naing Myo Thaw <naingmyothaw.nmyt@gmail.com>
2021-01-18 20:33:49 +0630

    first commit

---
+++ android/app/src/main/kotlin/com/then/flutter_melody/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.then.flutter_melody
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'ui/main_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Melody',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.red,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MainPage(),
+    );
+  }
+}
